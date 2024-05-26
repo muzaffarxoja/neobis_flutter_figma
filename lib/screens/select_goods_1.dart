84a709a703d6add32afda9758e4da4b2abbd9aa2
Muzaffar_Komil <muzsamgio@gmail.com>
2024-05-26 09:29:19 +0000

    group buttons add

--- lib/screens/select_goods_1.dart
+++ lib/screens/select_goods_1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:group_button/group_button.dart';
 
 class SelectGoods1 extends StatelessWidget {
   const SelectGoods1({super.key});
@@ -10,37 +11,57 @@ class SelectGoods1 extends StatelessWidget {
         title: Text(''),
       ),
       body: Container(
-        padding: EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(10),
         child: Column(
           children: [
             Text(
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
               'Выбрат товары',
             ),
-            SizedBox(height: 10),
-            Row(children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(width: 1)),
-                    hintText: 'Поиск',
-                    //labelText: "Email",
-                    prefixIcon: Icon(Icons.find_in_page_rounded),
-                    suffixIcon: IconButton(
-                      icon: Icon(Icons.data_array_outlined),
-                      onPressed: () {},
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(width: 1)),
+                      hintText: 'Поиск',
+                      //labelText: "Email",
+                      prefixIcon: Icon(Icons.find_in_page_rounded),
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.data_array_outlined),
+                        onPressed: () {},
+                      ),
                     ),
+                    keyboardType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.done,
                   ),
-                  keyboardType: TextInputType.emailAddress,
-                  textInputAction: TextInputAction.done,
                 ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.settings_accessibility),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            GroupButton(
+              options: GroupButtonOptions(
+                selectedTextStyle: TextStyle(
+                  fontSize: 10,
+                  color: Color(0xfff0e9ed),
+                ),
+                unselectedTextStyle: TextStyle(
+                  fontSize: 10,
+                ),
+                borderRadius: BorderRadius.circular(15),
+                mainGroupAlignment: MainGroupAlignment.start,
+                crossGroupAlignment: CrossGroupAlignment.start,
+                groupRunAlignment: GroupRunAlignment.start,
               ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.settings_accessibility),
-              ),
-            ])
+              isRadio: true,
+              buttons: ["моя точка", "Приход 12г", "Контейнер 454"],
+            )
           ],
         ),
       ),
